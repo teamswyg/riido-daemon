92bc0e661d4c4640d3354e839cb92f1ab72bc188
kimjooyoon <asketeddy@gmail.com>
2026-06-19 10:25:04 +0900

    Refactor policy bundle run test surface

--- internal/agentbridge/supervisor/policy_bundle_run_test.go
+++ internal/agentbridge/supervisor/policy_bundle_run_test.go
@@ -7,10 +7,7 @@ import (
 
 	"github.com/teamswyg/riido-daemon/internal/agentbridge/bridge"
 	"github.com/teamswyg/riido-daemon/internal/agentbridge/controlplane"
-	"github.com/teamswyg/riido-daemon/internal/agentbridge/runtimeactor"
-	"github.com/teamswyg/riido-daemon/internal/policy"
 	"github.com/teamswyg/riido-daemon/internal/process"
-	"github.com/teamswyg/riido-daemon/internal/workdir"
 )
 
 func runPolicyBundleScenario(t *testing.T, cfg policyBundleScenarioConfig) policyBundleScenario {
@@ -43,33 +40,4 @@ func runPolicyBundleScenario(t *testing.T, cfg policyBundleScenarioConfig) polic
 		running: running,
 		result:  completePolicyBundleTask(t, reporter, running),
 	}
-}
-
-func startPolicyBundleActor(
-	t *testing.T,
-	source *controlplane.MemorySource,
-	reporter *reporterProbe,
-	rt *runtimeactor.Actor,
-	bundle policy.PolicyBundle,
-) *Actor {
-	t.Helper()
-	actor, err := New(Config{
-		DaemonID:            "daemon-1",
-		Runtime:             rt,
-		Source:              source,
-		Reporter:            reporter,
-		Workdir:             workdir.NewFSAdapter(t.TempDir()),
-		PollEvery:           10 * time.Millisecond,
-		HeartbeatEvery:      time.Hour,
-		PolicyBundleVersion: bundle.Version,
-		PolicyBundle:        bundle,
-		RuntimeTrustTier:    policy.TrustTierHost,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := actor.Start(context.Background()); err != nil {
-		t.Fatal(err)
-	}
-	return actor
 }
