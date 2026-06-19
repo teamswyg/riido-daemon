74b5e5c95f9510cc6e99c54d40cd4b216a5d0dd4
kimjooyoon <115961382+kimjooyoon@users.noreply.github.com>
2026-06-19 09:28:54 +0900

    Refactor app data root test surface (#368)

--- internal/hostintegration/app_data_root_test.go
+++ internal/hostintegration/app_data_root_test.go
@@ -53,34 +53,6 @@ func TestDefaultAppDataRootMacAppStorePrefersAppGroup(t *testing.T) {
 	}
 }
 
-func TestDefaultAppDataRootMSIXStoreRequiresPackageLocalRoot(t *testing.T) {
-	_, err := DefaultAppDataRoot(AppDataRootInput{
-		Channel: DistributionChannelMSIXStore,
-		HostOS:  HostOSWindows,
-	})
-	if err == nil {
-		t.Fatal("expected msix store root without package local data to fail")
-	}
-}
-
-func TestDefaultAppDataRootMSIXStoreUsesWindowsSeparators(t *testing.T) {
-	root, err := DefaultAppDataRoot(AppDataRootInput{
-		Channel:                     DistributionChannelMSIXStore,
-		HostOS:                      HostOSWindows,
-		WindowsPackageLocalDataRoot: `C:\Users\tester\AppData\Local\Packages\Riido_abc\LocalState`,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if root.Scope != AppDataRootWindowsPackageLocal {
-		t.Fatalf("scope = %q, want %q", root.Scope, AppDataRootWindowsPackageLocal)
-	}
-	if root.WorkdirRoot() != `C:\Users\tester\AppData\Local\Packages\Riido_abc\LocalState\workspaces` {
-		t.Fatalf("workdir root = %q", root.WorkdirRoot())
-	}
-}
-
 func TestDefaultAppDataRootRejectsChannelOSMismatch(t *testing.T) {
 	_, err := DefaultAppDataRoot(AppDataRootInput{
 		Channel:                     DistributionChannelMacAppStore,
