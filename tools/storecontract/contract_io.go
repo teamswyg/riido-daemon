9854b08ebf9238a3d77db550d1c59d475cc777d4
kimjooyoon <115961382+kimjooyoon@users.noreply.github.com>
2026-06-19 13:15:13 +0900

    Refactor store distribution contract surface (#411)

--- tools/storecontract/contract_io.go
+++ tools/storecontract/contract_io.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func loadContract(path string) (contract, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return contract{}, fmt.Errorf("read contract %s: %w", path, err)
-	}
 	var loaded contract
-	if err := json.Unmarshal(data, &loaded); err != nil {
-		return contract{}, fmt.Errorf("decode contract %s: %w", path, err)
+	if err := decodeJSONFile(path, &loaded); err != nil {
+		return contract{}, err
+	}
+	for _, channelFile := range loaded.ChannelFiles {
+		var channel channel
+		shardPath := filepath.Join(filepath.Dir(path), channelFile)
+		if err := decodeJSONFile(shardPath, &channel); err != nil {
+			return contract{}, err
+		}
+		loaded.Channels = append(loaded.Channels, channel)
 	}
 	return loaded, nil
+}
+
+func decodeJSONFile(path string, target any) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("read contract %s: %w", path, err)
+	}
+	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(target); err != nil {
+		return fmt.Errorf("decode contract %s: %w", path, err)
+	}
+	return nil
 }
