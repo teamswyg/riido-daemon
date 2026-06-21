2d73f479a391d95b5357ba5c7cb0973fc1ad00c4
kimjooyoon <asketeddy@gmail.com>
2026-06-21 13:24:30 +0900

    Close daemon packaging and internal evidence loops

---
+++ tools/storedistributiondocs/types_loop.go
@@ -0,0 +1,9 @@
+package main
+
+type loopMeta struct {
+	Observation   string `json:"observation"`
+	Hypothesis    string `json:"hypothesis"`
+	Execute       string `json:"execute"`
+	Evaluate      string `json:"evaluate"`
+	Retrospective string `json:"retrospective"`
+}
